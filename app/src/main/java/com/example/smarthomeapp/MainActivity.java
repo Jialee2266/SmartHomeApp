351abdbe4ca9f8b142823139555f65eae861ccd3
jaden-letsgo <tl.li@hotmail.com>
2022-09-12 15:19:34 -0700

    Initial Commit

--- app/src/main/java/com/example/smarthomeapp/MainActivity.java
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -2,16 +2,46 @@ package com.example.smarthomeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new
+        List<String> fileList = new ArrayList<>();
+        Field[] fields=R.raw.class.getFields();
+        for(Field i : fields) {
+            fileList.add(i.getName());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, fileList);
+        AutoCompleteTextView dropdownMenu = findViewById(R.id.dropdown_menu);
+
+        dropdownMenu.setAdapter(adapter);
+        dropdownMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String value = fileList.get(i);
+                Intent intent = new Intent(MainActivity.this, VideoPlayer.class);
+                intent.putExtra("key", value);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
     }
 }
