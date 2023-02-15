0a7369db5248cd389c65aa63cfb297d5f437d1e0
jaden-letsgo <78634976+jaden-letsgo@users.noreply.github.com>
2023-02-14 16:11:36 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/smarthomeapp/MainActivity.java
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -4,12 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.*;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
