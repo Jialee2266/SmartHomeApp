7c11f7e54aaed9105f5a1bc23549f6b88e094870
jaden-letsgo <78634976+jaden-letsgo@users.noreply.github.com>
2023-02-14 16:19:00 -0800

    Removed Unused Import

--- app/src/main/java/com/example/smarthomeapp/VideoPlayer.java
+++ app/src/main/java/com/example/smarthomeapp/VideoPlayer.java
@@ -2,16 +2,11 @@ package com.example.smarthomeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.MediaController;
-import android.widget.TextView;
-import android.widget.VideoView;
+import android.widget.*;
 
 public class VideoPlayer extends AppCompatActivity {
     private String value = " ";
